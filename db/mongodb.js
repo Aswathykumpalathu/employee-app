b4f5f9c86c5695f5689b4fc91612557ca67e13f4
Aswathykumpalathu <aswathykumpalathu@gmail.com>
2023-07-20 13:24:49 +0300

    env removed

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect(process.env.uri)
+mongoose.connect("mongodb+srv://AswathyKiran:Pulser90@cluster0.aq7mdxr.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{
     console.log('connected to atls');
 })
